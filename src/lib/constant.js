665817d03f92fb19f9f4016ac969894ad6323d8e
unknown <pankaj.g@nipralo.com>
2025-05-19 14:45:19 +0530

    Port changes

--- src/lib/constant.js
+++ src/lib/constant.js
@@ -1,4 +1,4 @@
 
-export const SITE_URL = "http://localhost:3000/";
+// export const SITE_URL = "http://localhost:3000/";
 // export const SITE_URL = "http://192.168.29.111:3000/";
-// export const SITE_URL = "https://nipralo-jira-6j9w.vercel.app/";
+export const SITE_URL = "https://nipralo-jira-6j9w.vercel.app/";
